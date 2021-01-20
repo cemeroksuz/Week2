9b380bfc84fd5a8b4ac146066f90a8eb8cd2af34
cem eröksüz <cemeroksuz@outlook.com>
2021-01-20 23:58:39 +0300

    20 ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList <T>
+    {
+        T[] items;
+        
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //geçici Dizi - dizinin referans numarasını tutmak için
+            T[] tempArray = items;
+            //items'ın boyutunun 1 büyütülmesi (add ile gelen eleman için yer açıyoruz)
+            items = new T[items.Length + 1];
+            //geçici dizideki elemanların items'e aktarılması
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            //add ile gelen eleman items'in yeni açılan alanına yerleşiyor.
+            items[items.Length - 1] = item;
+            
+            
+
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //id
+        public int Id { get; set; }
+        //ketogeri id
+        public int CategoryId { get; set; }
+        //isim
+        public string ProductName { get; set; }
+        //fiyat
+        public double UnitPrice { get; set; }
+        //stok adedi
+        public int UnitInStock { get; set; }
+
+    }
+}
